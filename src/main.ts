33c77686769ca5d5c0c653e4b03ea119341b66d4
Phan Long Hieu <hieuflong@gmail.com>
2021-09-13 16:23:58 +0700

    Init Project - First commit

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(join(__dirname, '..', 'src/public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'src/views'));
+  app.setViewEngine('hbs');
+
+  await app.listen(3001);
+}
+bootstrap();
